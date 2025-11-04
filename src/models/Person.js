c6df345f2fe593f395580e05b830fa0b6c03106b
Darios2021 <perezruartedario@gmail.com>
2025-11-03 22:53:24 -0300

    asd112

--- src/models/Person.js
+++ src/models/Person.js
@@ -1,20 +1,26 @@
-// src/models/Person.js
+// src/models/Person.js  (FINAL)
 import { DataTypes, Model } from 'sequelize'
 import { sequelize } from '../db.js'
 
 export class Person extends Model {}
 
-Person.init({
-  id:          { type: DataTypes.INTEGER.UNSIGNED, primaryKey: true, autoIncrement: true },
-  name:        { type: DataTypes.STRING(120), allowNull: false },
-  doc:         { type: DataTypes.STRING(50) },
-  org:         { type: DataTypes.STRING(120) },
-  cargo:       { type: DataTypes.STRING(120) },
-  seat:        { type: DataTypes.STRING(10) },
-  present:     { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
-  presentAt:   { type: DataTypes.DATE },
-}, {
-  sequelize,
-  tableName: 'person',
-  timestamps: true,          // created_at / updated_at
-})
+Person.init(
+  {
+    id:        { type: DataTypes.INTEGER.UNSIGNED, primaryKey: true, autoIncrement: true },
+    name:      { type: DataTypes.STRING(120), allowNull: false },
+    doc:       { type: DataTypes.STRING(50) },
+    org:       { type: DataTypes.STRING(120) },
+    cargo:     { type: DataTypes.STRING(120) },
+    seat:      { type: DataTypes.STRING(10) },
+    present:   { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
+
+    // Usamos atributo camelCase, Sequelize lo mapea a 'present_at' por underscored:true
+    presentAt: { type: DataTypes.DATE, field: 'present_at' },
+  },
+  {
+    sequelize,
+    tableName: 'person',
+    timestamps: true,     // usa createdAt/updatedAt como atributos…
+    underscored: true,    // …pero los mapea a created_at / updated_at en la tabla
+  }
+)
