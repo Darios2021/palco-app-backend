330ee3e3c09ac3ae95094f195878d66bf2aa09fe
Darios2021 <perezruartedario@gmail.com>
2025-11-03 21:16:32 -0300

    asd123412

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 // src/routes/index.js
 import { Router } from 'express'
+import authRoutes from './auth.js'
 import peopleRoutes from './people.js'
 import palcosRoutes from './palcos.js'
 
 const r = Router()
 
+r.use('/auth', authRoutes)     // <— ¡esto faltaba!
 r.use('/people', peopleRoutes)
 r.use('/palcos', palcosRoutes)
 
